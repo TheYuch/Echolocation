510f7842e9dfd08908d5a099c98e92a2fb8d776b
Yuchen Xin <xin.yuchen@hotmail.com>
2023-10-14 15:59:44 -0700

    Add: Create, Edit: Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import Create from './Create.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Echolocation</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <Create />
     </>
   )
 }
