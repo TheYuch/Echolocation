9feb67e40df1275e473ea2e4a7b300bb732b16e2
Yuchen Xin <xin.yuchen@hotmail.com>
2023-10-15 11:19:06 -0700

    Edit: Change Server URL

--- src/contexts/Socket.jsx
+++ src/contexts/Socket.jsx
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'http://localhost:8000'; // TODO change to server address
+const SOCKET_URL = 'https://stoked-champion-402109.wl.r.appspot.com/';
 
 export const socket = io(SOCKET_URL);
