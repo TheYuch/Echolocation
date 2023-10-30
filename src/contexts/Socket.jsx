85716dfefcfad7adfb14d6c85b4c0f51b8ed8fce
Yuchen Xin <xin.yuchen@hotmail.com>
2023-10-29 21:49:36 -0700

    Edit: Project configuration

--- src/contexts/Socket.jsx
+++ src/contexts/Socket.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = 'https://stoked-champion-402109.wl.r.appspot.com/';
-// const SOCKET_URL = 'http://localhost:8000';
+const SOCKET_URL = 'https://echolocation-server-jgwu2mlq5q-wl.a.run.app';
+// const SOCKET_URL = 'http://localhost:8000'; // For local server only
 
 export const socket = io(SOCKET_URL);
